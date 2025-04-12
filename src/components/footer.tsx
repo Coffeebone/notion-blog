ed55ec23eeba9bda34938d54889328907e091853
Coffeebone <77718835+Coffeebone@users.noreply.github.com>
2025-04-12 11:55:11 +0900

    Update footer.tsx

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -4,6 +4,21 @@ export default function Footer() {
   return (
     <>
       <footer>
+        <span>후원해주세요!</span>
+        <ExtLink href="https://buymeacoffee.com/coffeebone">
+          <img
+            src="https://indify.co/widgets/live/button/oVIt19ceMDvSoFh07H1w"
+            height={46}
+            width={132}
+            alt="buy me a coffee!"
+          />
+        </ExtLink>
+        <span>
+          or{' '}
+          <ExtLink href="https://github.com/ijjk/notion-blog">
+            view source
+          </ExtLink>
+        </span>
       </footer>
     </>
   )
