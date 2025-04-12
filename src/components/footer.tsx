48a86c657aa11a20e252d6b6af14be4a56b85e18
Coffeebone <77718835+Coffeebone@users.noreply.github.com>
2025-04-12 11:48:24 +0900

    Update footer.tsx

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -4,21 +4,6 @@ export default function Footer() {
   return (
     <>
       <footer>
-        <span>후원해주세요!</span>
-        <ExtLink href="https://buymeacoffee.com/coffeebone">
-          <img
-            src="https://indify.co/widgets/live/button/oVIt19ceMDvSoFh07H1w"
-            height={46}
-            width={132}
-            alt="buy me a coffee!"
-          />
-        </ExtLink>
-        <span>
-          or{' '}
-          <ExtLink href="https://buymeacoffee.com/coffeebone">
-            view source
-          </ExtLink>
-        </span>
       </footer>
     </>
   )
