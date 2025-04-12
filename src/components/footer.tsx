5128aeee4c319cf29c0f39f7835e60a5c6d65595
Coffeebone <77718835+Coffeebone@users.noreply.github.com>
2025-04-12 22:37:48 +0900

    Update footer.tsx

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,24 +1,21 @@
-import ExtLink from './ext-link'
+// import ExtLink from './ext-link' // ExtLink는 더 이상 사용되지 않으므로 주석 처리하거나 삭제합니다.
 
 export default function Footer() {
   return (
     <>
       <footer>
         <span>후원해주세요!</span>
-        <ExtLink href="https://buymeacoffee.com/coffeebone">
-          <img
-            src="https://indify.co/widgets/live/button/oVIt19ceMDvSoFh07H1w"
-            height={46}
-            width={132}
-            alt="buy me a coffee!"
-          />
-        </ExtLink>
-        <span>
-          or{' '}
-          <ExtLink href="https://github.com/ijjk/notion-blog">
-            view source
-          </ExtLink>
-        </span>
+        <iframe
+          src="https://indify.co/widgets/live/button/GSUUhCgQrbSjtyf0ObVa" // 제공된 Indify 위젯 URL
+          style={{
+            border: '0', // iframe 테두리 제거
+            width: '210px', // 위젯의 기본 너비 (필요시 조절)
+            height: '50px', // 위젯의 기본 높이 (필요시 조절)
+            display: 'block', // 가운데 정렬 등을 위해 block 요소로 만듬 (선택사항)
+            margin: '0 auto'   // 위젯을 가운데 정렬 (선택사항, 원치 않으면 제거)
+          }}
+          title="Indify Widget Button" // 접근성을 위한 제목
+        ></iframe>
       </footer>
     </>
   )
